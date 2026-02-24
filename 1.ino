2da8700fa6eea05df74a03d6bf0e6f35e6099eae
Mohammod Hasan Al Muttaki <hasanalmuttaki@gmail.com>
2026-02-24 19:22:59 +0600

    first commit

---
+++ 1.ino
@@ -0,0 +1,18 @@
+const int IR_PIN = 2;   // connect IR OUT to pin 2
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(IR_PIN, INPUT);
+}
+
+void loop() {
+  int sensorValue = digitalRead(IR_PIN);
+
+  if (sensorValue == LOW) {
+    Serial.println("IR Sensor: FULL (Object Detected)");
+  } else {
+    Serial.println("IR Sensor: EMPTY (No Object)");
+  }
+
+  delay(500);
+}
