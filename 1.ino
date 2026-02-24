8d951e281419e011474a2fa8051bc72397463c00
Mohammod Hasan Al Muttaki <hasanalmuttaki@gmail.com>
2026-02-25 02:43:49 +0600

    the main car entering function is DONE

--- 1.ino
+++ 1.ino
@@ -2,14 +2,66 @@
 
 Servo myServo;
 
+const int IR1 = 2;
+const int IR2 = 3;
+const int IR3 = 5;  // checking sensor
+
+int slot1;
+int slot2;
+
 void setup() {
-  myServo.attach(9);   // servo signal pin connected to D9
+  Serial.begin(9600);
+
+  myServo.attach(9);
+
+  pinMode(IR1, INPUT);
+  pinMode(IR2, INPUT);
+  pinMode(IR3, INPUT);
+}
+
+// ✅ function must return int
+int checkIR(int pin, const char* name) {
+  if (digitalRead(pin) == LOW) {
+    Serial.print(name);
+    Serial.println(" FULL");
+    return 0;   // slot occupied
+  } else {
+    Serial.print(name);
+    Serial.println(" EMPTY");
+    return 1;   // slot free
+  }
 }
 
 void loop() {
-  myServo.write(0);    // move to 0 degrees
-  delay(1000);         // wait 1 second
 
-  myServo.write(90);   // move to 90 degrees
-  delay(1000);         // wait 1 second
+  if (digitalRead(IR3) == LOW) {
+    Serial.println("CAR IS WAITING FOR ENTER");
+    Serial.println("------");
+    slot1 = checkIR(IR1, "IR1");
+    slot2 = checkIR(IR2, "IR2");
+    Serial.println("------");
+
+    int totalEmpty = slot1 + slot2;
+
+    Serial.print("Available Slots: ");
+    Serial.println(totalEmpty);
+
+    if (totalEmpty == 0) {
+      Serial.println("PARKING FULL - GATE CLOSED");
+      myServo.write(0);
+    } else {
+      Serial.println("SPACE AVAILABLE - GATE OPEN");
+      myServo.write(90);
+      delay(5000);
+      myServo.write(0);
+      Serial.println("GATE CLOSED");
+    }
+    delay(2000);
+  }
+
+  else {
+    Serial.println("NO CAR ENTERING");
+    Serial.println("------");
+    delay(2000);
+  }
 }
