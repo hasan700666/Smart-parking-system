26f7748f99c328c19ec4a3a930c2bc841a0e70d1
Mohammod Hasan Al Muttaki <hasanalmuttaki@gmail.com>
2026-02-25 17:14:13 +0600

    add slot 2 to 6

--- 1.ino
+++ 1.ino
@@ -5,14 +5,19 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 Servo myServo;
 
+// Slot sensors
 const int IR1 = 2;
 const int IR2 = 3;
-const int IRI = 12;   // entry sensor
-const int IRO = 13;   // exit sensor
+const int IR3 = 4;
+const int IR4 = 5;
+const int IR5 = 6;
+const int IR6 = 7;
 
-int slot1;
-int slot2;
+// Entry & Exit
+const int IRI = 12;
+const int IRO = 13;
 
+int s1, s2, s3, s4, s5, s6;
 bool gateOpen = false;
 
 void setup() {
@@ -23,6 +28,11 @@ void setup() {
 
   pinMode(IR1, INPUT);
   pinMode(IR2, INPUT);
+  pinMode(IR3, INPUT);
+  pinMode(IR4, INPUT);
+  pinMode(IR5, INPUT);
+  pinMode(IR6, INPUT);
+
   pinMode(IRI, INPUT);
   pinMode(IRO, INPUT);
 
@@ -50,47 +60,65 @@ void closeGate() {
 
 void loop() {
 
-  // ---- SLOT STATUS ----
-  slot1 = checkIR(IR1);
-  slot2 = checkIR(IR2);
-  int totalEmpty = slot1 + slot2;
+  // ---- SLOT CHECK ----
+  s1 = checkIR(IR1);
+  s2 = checkIR(IR2);
+  s3 = checkIR(IR3);
+  s4 = checkIR(IR4);
+  s5 = checkIR(IR5);
+  s6 = checkIR(IR6);
 
-  Serial.print("Free: ");
+  int totalEmpty = s1 + s2 + s3 + s4 + s5 + s6;
+
+  Serial.print("Free Slots: ");
   Serial.println(totalEmpty);
 
-  // ---- ENTRY EVENT ----
+  // ---- ENTRY ----
   if (digitalRead(IRI) == LOW && !gateOpen) {
-    if (totalEmpty > 0) {
-      openGate();
-    } else {
-      Serial.println("PARKING FULL");
-    }
+    if (totalEmpty > 0) openGate();
+    else Serial.println("PARKING FULL");
   }
 
-  // ---- EXIT EVENT ----
+  // ---- EXIT ----
   if (digitalRead(IRO) == LOW && !gateOpen) {
     openGate();
   }
 
-  // ---- CLOSE CONDITION ----
+  // ---- CLOSE ----
   if (gateOpen && digitalRead(IRI) == HIGH && digitalRead(IRO) == HIGH) {
     delay(1000);
     closeGate();
   }
 
-  // ---- LCD ----
-  lcd.setCursor(0,0);
+  // ---- LCD PAGE 1 (S1–S3) ----
+  lcd.setCursor(1,0);
   lcd.print("S1:");
-  lcd.print(slot1 == 0 ? "F" : "E");
+  lcd.print(s1==0?"F":"E");
+  lcd.print(" S2:");
+  lcd.print(s2==0?"F":"E");
+  lcd.print(" S3:");
+  lcd.print(s2==0?"F":"E");
 
-  lcd.setCursor(8,0);
-  lcd.print("S2:");
-  lcd.print(slot2 == 0 ? "F" : "E");
+  lcd.setCursor(1,1);
+  lcd.print("S4:");
+  lcd.print(s4==0?"F":"E");
+  lcd.print(" S5:");
+  lcd.print(s5==0?"F":"E");
+  lcd.print(" S6:");
+  lcd.print(s6==0?"F":"E");
+  delay(4000);
+
+  // ---- LCD PAGE 2 (S4–S6) ----
+  lcd.clear();
+
+  lcd.setCursor(0,0);
+  lcd.print("     Free:");
+  lcd.print(totalEmpty);
 
   lcd.setCursor(0,1);
-  lcd.print("Free:");
-  lcd.print(totalEmpty);
-  lcd.print("   ");
+  lcd.print("   Thank  You");
+  lcd.print(" ");
+  delay(2000);
 
-  delay(300);
+  lcd.clear();
 }
